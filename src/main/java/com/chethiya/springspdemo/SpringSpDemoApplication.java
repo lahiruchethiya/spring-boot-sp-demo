f496d13c01d625a0b0e95ab573e6374a56a00aa6
lahiruchethiya <lahiruchethiya.jayasinghe@gmail.com>
2023-01-29 17:21:48 +0530

    Initial commit

---
+++ src/main/java/com/chethiya/springspdemo/SpringSpDemoApplication.java
@@ -0,0 +1,13 @@
+package com.chethiya.springspdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSpDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSpDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chethiya/springspdemo/SpringSpDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chethiya.springspdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSpDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
